37f81f9f9ae34f5cefc8fbc15c37947ada6c1fb0
septyani <septyani@users.noreply.github.com>
2019-11-05 10:43:06 +0700

    Version 4
    This is the version 4

--- main.java
+++ main.java
@@ -4,8 +4,8 @@ package pegadaianGIT;
 
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
-				System.out.println("Ini version 3");
-				System.out.println("Hello World3");
+				System.out.println("Ini version 4");
+				System.out.println("Hello World4");
 		}
 
 	}
