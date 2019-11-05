a1fd9fa7998ec9be641cc2b3364568c5a181636c
septyani <septyani@users.noreply.github.com>
2019-11-05 09:56:59 +0700

    Version 1
    This is version 1

---
+++ main.java
@@ -0,0 +1,13 @@
+package pegadaianGIT;
+
+	public class main {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+				System.out.println("Version 1");
+				System.out.println("Hello World1");
+		}
+
+	}
+
+
