fdad0471ad9e26ede179a5b9eececce8073da815
septyani <septyani@users.noreply.github.com>
2019-11-05 10:43:40 +0700

    Version 5
    Ini Version 5

--- main.java
+++ main.java
@@ -4,8 +4,8 @@ package pegadaianGIT;
 
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
-				System.out.println("Ini version 4");
-				System.out.println("Hello World4");
+				System.out.println("This is version 5.0");
+				System.out.println("Hello World 5");
 		}
 
 	}
