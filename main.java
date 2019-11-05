f15c8f685b412c58e39a3a3f527d50f5ecbec8c0
septyani <septyani@users.noreply.github.com>
2019-11-05 10:01:59 +0700

    Version 2
    This is version 2

--- main.java
+++ main.java
@@ -4,8 +4,8 @@ package pegadaianGIT;
 
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
-				System.out.println("Version 1");
-				System.out.println("Hello World1");
+				System.out.println("Version 2");
+				System.out.println("Hello World2");
 		}
 
 	}
