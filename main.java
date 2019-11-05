76b2141fa051555e8ff5d67af069c3e69dfd5f18
septyani <septyani@users.noreply.github.com>
2019-11-05 10:42:35 +0700

    Version 3
    Ini version 3

--- main.java
+++ main.java
@@ -4,8 +4,8 @@ package pegadaianGIT;
 
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
-				System.out.println("Version 2");
-				System.out.println("Hello World2");
+				System.out.println("Ini version 3");
+				System.out.println("Hello World3");
 		}
 
 	}
