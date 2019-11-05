e71d0ef7c313f6baa39921203a4afce323d844e8
septyani <septyani@users.noreply.github.com>
2019-11-05 11:06:59 +0700

    Version 2

---
+++ main - Copy.java
@@ -0,0 +1,13 @@
+package pegadaianGIT;
+
+	public class main {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+				System.out.println("This is version 2.0");
+				System.out.println("Hello World 2");
+		}
+
+	}
+
+
